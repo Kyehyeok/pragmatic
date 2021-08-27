478ff94543afeae94931611f38a0848318141d3b
kyehyeok <laaa391919@gmail.com>
2021-08-27 10:11:55 +0900

    django course 29  commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -139,6 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -146,3 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy("accountapp:hello_world")
 LOGOUT_REDIRECT_URL = reverse_lazy("accountapp:login")
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
