492769f01a829ebc954f8ad0ea47071bc5c5ae36
kyehyeok <laaa391919@gmail.com>
2021-09-06 16:37:41 +0900

    django course 36  commit

--- profileapp/models.py
+++ profileapp/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-
+    # 아래 3개는  forms.py에서 필드 지정
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
     message = models.CharField(max_length=100, null=True)
