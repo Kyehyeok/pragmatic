7ea46c6c2ab9bf78f24fd1dc6c64200d0ab6143e
kyehyeok <laaa391919@gmail.com>
2021-09-24 18:27:26 +0900

    try to find error4

--- pragmatic/settings/deploy.py
+++ pragmatic/settings/deploy.py
@@ -27,8 +27,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'password1234',
+        'HOST': 'mariadb',
+        'PORT': '3306',
     }
 }
-
